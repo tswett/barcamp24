50f111650b65878eda1a0331f20f14e146d12d61
Tanner Swett <tannerswett@gmail.com>
2024-07-31 15:57:42 -0400

    Attempt to get UART transmission working (it doesn't)

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,40 @@ mod peripherals;
 use cortex_m_rt::entry;
 use panic_halt as _;
 
-use peripherals::{GpioMode, GPIOG, RCC_AHB1ENR};
+use peripherals::{AltFunc, GpioMode, GPIOA, GPIOG, RCC_AHB1ENR, RCC_APB2ENR, USART1};
+
+const SYSCLK_SPEED: u32 = 16_000_000;
 
 #[entry]
 fn main() -> ! {
+    RCC_AHB1ENR.enable_gpioa();
     RCC_AHB1ENR.enable_gpiog();
+    // RCC_AHB1ENR.enable!(GPIOA, GPIOG);
+
+    RCC_APB2ENR.enable_usart1();
+
     GPIOG.set_mode(13, GpioMode::Output);
     GPIOG.set_high(13);
 
+    uart1_set_up();
+
+    // As a bit of a hack, write '.' 32 times as a delay in order to allow the display controller to come up
+    for _ in 0..32 {
+        USART1.transmit_byte('.' as u8);
+    }
+
     loop {}
+}
+
+fn uart1_set_up() {
+    GPIOA.set_alt_func(9, AltFunc::AF7);
+    GPIOA.set_alt_func(10, AltFunc::AF7);
+    // GPIOA.set_alt_funcs!(P9: AF7, P10: AF7);
+
+    GPIOA.set_mode(9, GpioMode::AltFunc);
+    GPIOA.set_mode(10, GpioMode::AltFunc);
+    // GPIOA.set_modes!(P9: AltFunc, P10: AltFunc);
+
+    USART1.set_brr((SYSCLK_SPEED / 9600) as u16);
+    USART1.enable_rx_tx();
 }
--- src/peripherals.rs
+++ src/peripherals.rs
@@ -1,3 +1,31 @@
+pub struct Register16 {
+    addr: u32,
+}
+
+impl Register16 {
+    pub unsafe fn read(&self) -> u16 {
+        let ptr = self.addr as *const u16;
+        ptr.read_volatile()
+    }
+
+    pub unsafe fn write(&self, value: u16) {
+        let ptr = self.addr as *mut u16;
+        ptr.write_volatile(value);
+    }
+
+    pub unsafe fn write_or(&self, value: u16) {
+        let old_reg_value = self.read();
+        let new_reg_value = old_reg_value | value;
+        self.write(new_reg_value);
+    }
+
+    pub unsafe fn write_masked(&self, mask: u16, value: u16) {
+        let old_reg_value = self.read();
+        let new_reg_value = (old_reg_value & !mask) | (value & mask);
+        self.write(new_reg_value);
+    }
+}
+
 pub struct Register32 {
     addr: u32,
 }
@@ -29,16 +57,36 @@ impl Register32 {
 pub struct RccAhb1enr;
 
 impl RccAhb1enr {
-    pub fn enable_gpiog(&self) {
-        let reg = Register32 { addr: 0x40023830 };
+    const REG: Register32 = Register32 { addr: 0x40023830 };
 
+    pub fn enable_gpioa(&self) {
         unsafe {
-            reg.write_or(1 << 6);
+            Self::REG.write_or(1 << 0);
+        }
+    }
+
+    pub fn enable_gpiog(&self) {
+        unsafe {
+            Self::REG.write_or(1 << 6);
         }
     }
 }
 
 pub const RCC_AHB1ENR: RccAhb1enr = RccAhb1enr;
+
+pub struct RccApb2enr;
+
+impl RccApb2enr {
+    const REG: Register32 = Register32 { addr: 0x40023844 };
+
+    pub fn enable_usart1(&self) {
+        unsafe {
+            Self::REG.write_or(1 << 4);
+        }
+    }
+}
+
+pub const RCC_APB2ENR: RccApb2enr = RccApb2enr;
 
 pub struct Gpio {
     base: u32,
@@ -63,10 +111,73 @@ impl Gpio {
             bsrr.write(value);
         }
     }
+
+    pub fn set_alt_func(&self, pin: u8, alt_func: AltFunc) {
+        let afr: Register32;
+        let offset: u8;
+
+        if pin < 8 {
+            afr = Register32 { addr: self.base + 0x20 };
+            offset = pin * 4;
+        } else {
+            afr = Register32 { addr: self.base + 0x24 };
+            offset = (pin - 8) * 4;
+        }
+
+        let mask = 0b1111 << offset;
+        let value = (alt_func as u32) << offset;
+
+        unsafe {
+            afr.write_masked(mask, value);
+        }
+    }
 }
 
+pub const GPIOA: Gpio = Gpio { base: 0x40020000 };
 pub const GPIOG: Gpio = Gpio { base: 0x40021800 };
 
 pub enum GpioMode {
     Output = 0b01,
+    AltFunc = 0b10,
 }
+
+pub enum AltFunc {
+    AF7 = 7,
+}
+
+pub struct Usart {
+    base: u32,
+}
+
+impl Usart {
+    pub fn set_brr(&self, value: u16) {
+        let brr = Register16 { addr: self.base + 0x08 };
+
+        unsafe {
+            brr.write(value);
+        }
+    }
+
+    pub fn enable_rx_tx(&self) {
+        let cr1 = Register16 { addr: self.base + 0x00 };
+        let ue_enable = 1 << 13;
+        let te_enable = 1 << 3;
+        let re_enable = 1 << 2;
+
+        unsafe {
+            cr1.write_or(ue_enable | te_enable | re_enable);
+        }
+    }
+
+    pub fn transmit_byte(&self, byte: u8) {
+        let sr = Register16 { addr: self.base + 0x00 };
+        let dr = Register16 { addr: self.base + 0x04 };
+
+        unsafe {
+            while sr.read() & (1 << 7) == 0 {}
+            dr.write(byte as u16);
+        }
+    }
+}
+
+pub const USART1: Usart = Usart { base: 0x40011000 };
